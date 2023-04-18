2abd69dfc6ddddc7c403fd064eaa5e63cd91a991
Henry Ding <henryrding@yahoo.com>
2023-04-18 21:24:33 +0000

    Added routes for planned paths as comments. #1

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,13 @@ import {Route, Routes} from 'react-router-dom';
 import './App.css';
 import NavBar from './components/NavBar';
 import Catalog from './pages/Catalog';
-import ProductDetails from './pages/ProductDetails';
-import Cart from './pages/Cart';
-import Auth from './pages/AuthPage';
-import Inventory from './pages/Inventory';
-import Orders from './pages/Orders';
-import OrderDetails from './pages/OrderDetails';
-import NotFound from './pages/NotFound';
+// import ProductDetails from './pages/ProductDetails';
+// import Cart from './pages/Cart';
+// import Auth from './pages/AuthPage';
+// import Inventory from './pages/Inventory';
+// import Orders from './pages/Orders';
+// import OrderDetails from './pages/OrderDetails';
+// import NotFound from './pages/NotFound';
 
 function App() {
   // const [serverData, setServerData] = useState("");
@@ -32,14 +32,14 @@ function App() {
       <Routes>
         <Route path="/" element={<NavBar />}>
           <Route index element={<Catalog />} />
-          <Route path="details/:cardId" element={<ProductDetails />} />
+          {/* <Route path="details/:cardId" element={<ProductDetails />} />
           <Route path="sign-in" element={<Auth action="sign-in" />} />
           <Route path="sign-up" element={<Auth action="sign-up" />} />
           <Route path="cart" element={<Cart />} />
           <Route path="orders" element={<Orders />} />
           <Route path="orderDetails/:orderNumber" element={<OrderDetails />} />
           <Route path="inventory" element={<Inventory />} />
-          <Route path="*" element={<NotFound />} />
+          <Route path="*" element={<NotFound />} /> */}
         </Route>
       </Routes>
   );
