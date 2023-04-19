292c22babc3f02fa5a3ee844334d319556cc16a8
Henry Ding <henryrding@yahoo.com>
2023-04-19 20:37:44 +0000

    Added key to <br />'s to get rid of console error. #2

--- client/src/lib/create-line-breaks.js
+++ client/src/lib/create-line-breaks.js
@@ -1,5 +1,5 @@
 export default function createLineBreaks(string) {
   let array = string.split('\\n');
-  const withLineBreaks = array.reduce((acc, curr) => acc.concat(curr, <br />), []).slice(0, -1);
+  const withLineBreaks = array.reduce((acc, curr, index) => acc.concat(curr, <br key={index} />), []).slice(0, -1);
   return withLineBreaks;
 }
