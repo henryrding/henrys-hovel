bfa840847a2e2c190c9dd64a59263ecc1b0d3f63
Henry Ding <henryrding@yahoo.com>
2023-04-21 17:42:37 +0000

    Updated App component with Cart route and component. #4

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import NavBar from './components/NavBar';
 import { ShopContextProvider } from './components/ShopContext';
 import Catalog from './pages/Catalog';
 import CardDetails from './pages/CardDetails';
-// import Cart from './pages/Cart';
+import Cart from './pages/Cart';
 // import Auth from './pages/AuthPage';
 // import Inventory from './pages/Inventory';
 // import Orders from './pages/Orders';
@@ -35,9 +35,9 @@ function App() {
         <Route path="/" element={<NavBar />}>
           <Route index element={<Catalog />} />
           <Route path="details/:cardId" element={<CardDetails />} />
+          <Route path="cart" element={<Cart />} />
           {/* <Route path="sign-in" element={<Auth action="sign-in" />} />
           <Route path="sign-up" element={<Auth action="sign-up" />} />
-          <Route path="cart" element={<Cart />} />
           <Route path="orders" element={<Orders />} />
           <Route path="orderDetails/:orderNumber" element={<OrderDetails />} />
           <Route path="inventory" element={<Inventory />} /> */}
