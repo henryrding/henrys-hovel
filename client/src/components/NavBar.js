644af2ad62f75ad498292e4d7d47e1cfd8449fbb
Henry Ding <henryrding@yahoo.com>
2023-04-24 04:25:04 +0000

    Updated NavBar component to show different nav-links if a user is signed in and if user is an admin. #7

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,10 +1,12 @@
+import { useContext } from 'react';
 import {Link, Outlet} from 'react-router-dom';
-// import {FaShieldAlt, FaUser, FaShoppingCart, FaSignInAlt, FaSignOutAlt} from 'react-icons/fa';
-import {FaUser, FaShoppingCart, FaSignInAlt} from 'react-icons/fa';
-// import { BsDatabaseFillAdd } from 'react-icons/bs';
+import { ShopContext } from './ShopContext';
+import {FaShieldAlt, FaUser, FaShoppingCart, FaSignInAlt, FaSignOutAlt} from 'react-icons/fa';
+import { BsDatabaseFillAdd } from 'react-icons/bs';
 import './NavBar.css';
 
 export default function NavBar() {
+  const {user, handleSignOut} = useContext(ShopContext);
 
   return (
     <>
@@ -16,42 +18,42 @@ export default function NavBar() {
           </Link>
           <div className="" id="navbarNav">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              {/* <li className="nav-item">
-                <Link className="nav-link" to="/orders">
-                  <FaShieldAlt size={36} />
-                  <span className="d-none d-md-inline ms-2 fw-semibold">Admin Tools</span>
-                </Link>
-              </li> */}
-              <li className="nav-item">
+              {!user?.isAdmin && <li className="nav-item">
                 <Link className="nav-link" to="/orders">
                   <FaUser size={36} />
                   <span className="d-none d-md-inline ms-2 fw-semibold">My Orders</span>
                 </Link>
-              </li>
-              <li className="nav-item">
+              </li>}
+              {user?.isAdmin && <li className="nav-item">
+                <Link className="nav-link" to="/orders">
+                  <FaShieldAlt size={36} />
+                  <span className="d-none d-md-inline ms-2 fw-semibold">Admin Tools</span>
+                </Link>
+              </li>}
+              {!user && <li className="nav-item">
                 <Link className="nav-link" to="/sign-in">
                   <FaSignInAlt size={36} />
                   <span className="d-none d-md-inline ms-2 fw-semibold">Sign In</span>
                 </Link>
-              </li>
-              {/* <li className="nav-item">
-                <Link className="nav-link" to="/">
+              </li>}
+              {user && <li className="nav-item">
+                <button className="nav-link btn" onClick={handleSignOut}>
                   <FaSignOutAlt size={36} />
                   <span className="d-none d-md-inline ms-2 fw-semibold">Sign Out</span>
-                </Link>
-              </li> */}
-              <li className="nav-item">
+                </button>
+              </li>}
+              {!user?.isAdmin && <li className="nav-item">
                 <Link className="nav-link" to="/cart">
                   <FaShoppingCart size={36} />
                   <span className="d-none d-md-inline ms-2 fw-semibold">Cart</span>
                 </Link>
-              </li>
-              {/* <li className="nav-item">
+              </li>}
+              {user?.isAdmin && <li className="nav-item">
                 <Link className="nav-link" to="/inventory">
                   <BsDatabaseFillAdd size={36} />
                   <span className="d-none d-md-inline ms-2 fw-semibold">Add Inventory</span>
                 </Link>
-              </li> */}
+              </li>}
             </ul>
           </div>
         </div>
