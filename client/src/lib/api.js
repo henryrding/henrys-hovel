c2c505d3426fb6b968177fab5e0a34eafa3aea20
Henry Ding <henryrding@yahoo.com>
2023-04-25 16:36:51 +0000

    Updated api.js with fetchCartInventory, addCartInventory, updateCartInventory, deleteCartInventory, and clearCartInventory functions. #9

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -29,3 +29,99 @@ export async function signUpOrIn(action, username, password, firstName, lastName
   }
   return await res.json();
 }
+
+export async function fetchCartInventory() {
+  const token = localStorage.getItem('tokenKey');
+  if (!token) {
+    throw new Error('Token not found');
+  }
+  const res = await fetch('/api/cartInventory', {
+    headers: {
+      'Authorization': `Bearer ${token}`
+    }
+  });
+  if (!res.ok) throw new Error(`fetch Error ${res.status}`);
+  return await res.json();
+}
+
+export async function addCartInventory(inventoryId, quantity) {
+  const token = localStorage.getItem('tokenKey');
+  if (!token) {
+    throw new Error('Token not found');
+  }
+  const req = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
+    },
+    body: JSON.stringify({ inventoryId, quantity })
+  };
+  const res = await fetch('/api/cartInventory', req);
+  if (!res.ok) {
+    const message = await res.text(res.body)
+    throw new Error(`${message.substring(10, message.length - 2)}`);
+  }
+  return await res.json();
+}
+
+export async function updateCartInventory(inventoryId, quantity) {
+  const token = localStorage.getItem('tokenKey');
+  if (!token) {
+    throw new Error('Token not found');
+  }
+  const req = {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`,
+    },
+    body: JSON.stringify({ quantity }),
+  };
+  const res = await fetch(`/api/cartInventory/${inventoryId}`, req);
+  if (!res.ok) {
+    const message = await res.text(res.body);
+    throw new Error(`${message.substring(10, message.length - 2)}`);
+  }
+  return await res.json();
+}
+
+export async function deleteCartInventory(inventoryId) {
+  const token = localStorage.getItem('tokenKey');
+  if (!token) {
+    throw new Error('Token not found');
+  }
+  const req = {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`,
+    },
+  };
+  const res = await fetch(`/api/cartInventory/${inventoryId}`, req);
+  if (!res.ok) {
+    const message = await res.text(res.body);
+    throw new Error(`${message.substring(10, message.length - 2)}`);
+  }
+  return await res.json();
+}
+
+export async function clearCartInventory() {
+  const token = localStorage.getItem('tokenKey');
+  if (!token) {
+    throw new Error('Token not found');
+  }
+  const req = {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`,
+    },
+  };
+  const res = await fetch(`/api/cartInventory`, req);
+  if (!res.ok) {
+    const message = await res.text(res.body);
+    throw new Error(`${message.substring(10, message.length - 2)}`);
+  }
+  return await res.json();
+}
