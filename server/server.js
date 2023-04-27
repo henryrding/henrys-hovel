2de8e5c016f42013022095c14452be07e8963134
Henry Ding <henryrding@yahoo.com>
2023-04-27 06:26:58 +0000

    Updated server with stripe, created middleware for checkout and to listen for successful checkout with stripe, and combined some sql queries. #10

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ import 'dotenv/config';
 import express from 'express';
 import argon2 from 'argon2';
 import jwt from 'jsonwebtoken';
+import Stripe from 'stripe';
+import bodyParser from 'body-parser';
 import ClientError from './lib/client-error.js';
 import errorMiddleware from './lib/error-middleware.js';
 import authorizationMiddleware from './lib/authorization-middleware.js';
@@ -16,7 +18,7 @@ const db = new pg.Pool({
 });
 
 const app = express();
-
+const stripe = new Stripe(process.env.STRIPE_PRIVATE_KEY);
 // Create paths for static directories
 const reactStaticDir = new URL('../client/build', import.meta.url).pathname;
 const uploadsStaticDir = new URL('public', import.meta.url).pathname;
@@ -31,6 +33,7 @@ app.get('/api/inventory', async (req, res, next) => {
     const sql = `
       select *
         from "inventory"
+        order by "inventoryId"
     `;
     const result = await db.query(sql);
     res.json(result.rows);
@@ -126,27 +129,171 @@ app.post('/api/auth/sign-in', async (req, res, next) => {
   }
 });
 
+app.post('/create-checkout-session/:userId', async (req, res, next) => {
+  try {
+    const { userId } = req.params;
+    const sql = `
+    select "inventory"."price",
+       "inventory"."name",
+       "cartInventory"."quantity"
+    from "inventory"
+    join "cartInventory" using ("inventoryId")
+    join "carts" using ("cartId")
+    join "users" using ("userId")
+    where "userId" = $1
+    `;
+    const params = [userId];
+    const result = await db.query(sql, params);
+    const cartItems = result.rows;
+    const session = await stripe.checkout.sessions.create({
+      shipping_address_collection: { allowed_countries: ['US', 'CA'] },
+      shipping_options: [
+        {
+          shipping_rate_data: {
+            type: 'fixed_amount',
+            fixed_amount: { amount: 0, currency: 'usd' },
+            display_name: 'Free shipping',
+            delivery_estimate: {
+              minimum: { unit: 'business_day', value: 5 },
+              maximum: { unit: 'business_day', value: 7 },
+            },
+          },
+        },
+        {
+          shipping_rate_data: {
+            type: 'fixed_amount',
+            fixed_amount: { amount: 1500, currency: 'usd' },
+            display_name: 'Next day air',
+            delivery_estimate: {
+              minimum: { unit: 'business_day', value: 1 },
+              maximum: { unit: 'business_day', value: 1 },
+            },
+          },
+        },
+      ],
+      line_items: cartItems.map(item => {
+        return {
+          price_data: {
+            currency: 'usd',
+            product_data: {
+              name: item.name
+            },
+            unit_amount: item.price
+          },
+          quantity: item.quantity
+        };
+      }),
+      client_reference_id: userId,
+      mode: 'payment',
+      success_url: 'http://localhost:3000/orders',
+      cancel_url: 'http://localhost:3000',
+    });
+
+    res.redirect(303, session.url);
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.post('/webhook', bodyParser.raw({ type: 'application/json' }), async (req, res, next) => {
+  const payload = req.body;
+  if (payload.type === 'checkout.session.completed') {
+    const totalPrice = payload.data.object.amount_total;
+    const shippingAddress = `${payload.data.object.shipping_details.address.line1} ${payload.data.object.shipping_details.address.line2 !== null ? payload.data.object.shipping_details.address.line2 + ' ' : ''}${payload.data.object.shipping_details.address.city}, ${payload.data.object.shipping_details.address.state} ${payload.data.object.shipping_details.address.postal_code} ${payload.data.object.shipping_details.address.country}`;
+    const shippingName = payload.data.object.shipping_details.name;
+    const shippingCost = payload.data.object.shipping_cost.amount_subtotal;
+    const userId = Number(payload.data.object.client_reference_id);
+    const orderNumber = payload.data.object.id;
+    try {
+      const sql = `
+        insert into "orders" ("orderNumber", "userId", "totalPrice", "shippingName", "shippingAddress", "shippingCost", "shipped")
+        values ($1, $2, $3, $4, $5, $6, $7)
+        returning *
+      `;
+      const params = [orderNumber, userId, totalPrice, shippingName, shippingAddress, shippingCost, false];
+      const result = await db.query(sql, params);
+      const [order] = result.rows;
+      const orderId = order.orderId;
+      const sql2 = `
+      insert into "orderItems" ("inventoryId", "orderId", "name", "collectorNumber", "setName", "setCode", "rarity", "foil", "price", "quantity", "cardId", "image", "manaCost", "typeLine", "oracleText", "power", "toughness", "flavorText", "artist", "visible" )
+      select "ci"."inventoryId",
+        "orders"."orderId",
+        "i"."name",
+        "i"."collectorNumber",
+        "i"."setName",
+        "i"."setCode",
+        "i"."rarity",
+        "i"."foil",
+        "i"."price",
+        "ci"."quantity",
+        "i"."cardId",
+        "i"."image",
+        "i"."manaCost",
+        "i"."typeLine",
+        "i"."oracleText",
+        "i"."power",
+        "i"."toughness",
+        "i"."flavorText",
+        "i"."artist",
+        "i"."visible"
+        from "inventory" as "i"
+        join "cartInventory" as "ci" using ("inventoryId")
+        join "carts" using ("cartId")
+        join "users" using ("userId")
+        join "orders" using ("userId")
+        where "userId" = $1 and "orderId" = $2
+      `;
+      const params2 = [userId, orderId];
+      const result2 = await db.query(sql2, params2);
+      const orderItems = result2.rows;
+      const sql3 = `
+      update "inventory"
+        set "quantity" = "inventory"."quantity" - "cartInventory"."quantity",
+            "visible" = CASE WHEN "inventory"."quantity" - "cartInventory"."quantity" <= 0 THEN false ELSE "inventory"."visible" END
+        from "cartInventory"
+        where "cartInventory"."inventoryId" = "inventory"."inventoryId"
+          and "cartInventory"."cartId" IN (
+            select "cartId"
+            from "carts"
+            where "userId" = $1
+          );
+      `;
+      const params3 = [userId];
+      const result3 = await db.query(sql3, params3);
+      const updatedInventory = result3.rows;
+      const sql4 = `
+      delete from "cartInventory"
+      where "cartId" in (
+        select "cartId"
+        from "carts"
+        where "userId" = $1
+      )
+      returning *
+    `;
+      const params4 = [userId];
+      const result4 = await db.query(sql4, params4);
+      const deletedItems = result4.rows;
+      res.status(200).json({ order, orderItems, updatedInventory, deletedItems });
+    } catch (err) {
+      next(err);
+    }
+  }
+});
+
 app.use(authorizationMiddleware);
 
 app.get('/api/cartInventory', async (req, res, next) => {
   try {
     const { userId } = req.user;
     const sql = `
-      select "cartId"
-        from "carts"
-        where "userId" = $1
+      select "cartInventory".*
+        from "cartInventory"
+        join "carts" using ("cartId")
+        where "userId" = $1;
     `;
     const params = [userId];
     const result = await db.query(sql, params);
-    const [cart] = result.rows;
-    const sql2 = `
-      select *
-        from "cartInventory"
-        where "cartId" = $1
-    `;
-    const params2 = [cart.cartId];
-    const result2 = await db.query(sql2, params2);
-    const cartInventory = result2.rows;
+    const cartInventory = result.rows;
     res.json(cartInventory);
   } catch (err) {
     next(err);
@@ -161,21 +308,17 @@ app.post('/api/cartInventory', async (req, res, next) => {
       throw new ClientError(400, 'inventoryId and quantity are required fields');
     }
     const sql = `
-      select "cartId"
-        from "carts"
-        where "userId" = $1
-    `;
-    const params = [userId];
-    const result = await db.query(sql, params);
-    const [cart] = result.rows;
-    const sql2 = `
       insert into "cartInventory" ("inventoryId", "cartId", "quantity")
-        values ($1, $2, $3)
-        returning *
+      values ($1, (
+        select "cartId"
+        from "carts"
+        where "userId" = $2
+      ), $3)
+      returning *
     `;
-    const params2 = [inventoryId, cart.cartId, quantity];
-    const result2 = await db.query(sql2, params2);
-    const [addedToCart] = result2.rows;
+    const params = [inventoryId, userId, quantity];
+    const result = await db.query(sql, params);
+    const [addedToCart] = result.rows;
     res.status(201).json(addedToCart);
   } catch (err) {
     next(err);
@@ -194,22 +337,18 @@ app.patch('/api/cartInventory/:inventoryId', async (req, res, next) => {
       throw new ClientError(400, 'quantity (number) is required');
     }
     const sql = `
-      select "cartId"
-        from "carts"
-        where "userId" = $1
-    `;
-    const params = [userId];
-    const result = await db.query(sql, params);
-    const [cart] = result.rows;
-    const sql2 = `
       update "cartInventory"
         set "quantity" = $1
-        where "inventoryId" = $2 and "cartId" = $3
+        where "cartId" in (
+          select "cartId"
+            from "carts"
+            where "userId" = $2
+        ) and "inventoryId" = $3
         returning *
     `;
-    const params2 = [quantity, inventoryId, cart.cartId];
-    const result2 = await db.query(sql2, params2);
-    const [updatedCartInventory] = result2.rows;
+    const params = [quantity, userId, inventoryId];
+    const result = await db.query(sql, params);
+    const [updatedCartInventory] = result.rows;
     if (!updatedCartInventory) {
       throw new ClientError(404, `cannot find card with inventoryId ${inventoryId} in cart`);
     }
@@ -225,27 +364,20 @@ app.delete('/api/cartInventory/:inventoryId', async (req, res, next) => {
     const { inventoryId } = req.params;
 
     const sql = `
-      select "cartId"
-        from "carts"
-        where "userId" = $1
-    `;
-    const params = [userId];
-    const result = await db.query(sql, params);
-    const [cart] = result.rows;
-
-    const sql2 = `
       delete from "cartInventory"
-      where "cartId" = $1 and "inventoryId" = $2
+      where "cartId" in (
+        select "cartId"
+          from "carts"
+          where "userId" = $1
+      ) and "inventoryId" = $2
       returning *
     `;
-    const params2 = [cart.cartId, inventoryId];
-    const result2 = await db.query(sql2, params2);
-    const [deletedItem] = result2.rows;
-
+    const params = [userId, inventoryId];
+    const result = await db.query(sql, params);
+    const [deletedItem] = result.rows;
     if (!deletedItem) {
       throw new ClientError(404, `Item with inventoryId ${inventoryId} not found in user's cart`);
     }
-
     res.json(deletedItem);
   } catch (err) {
     next(err);
@@ -257,22 +389,17 @@ app.delete('/api/cartInventory', async (req, res, next) => {
     const { userId } = req.user;
 
     const sql = `
-      select "cartId"
+      delete from "cartInventory"
+      where "cartId" in (
+        select "cartId"
         from "carts"
         where "userId" = $1
+      )
+      returning *
     `;
     const params = [userId];
     const result = await db.query(sql, params);
-    const [cart] = result.rows;
-
-    const sql2 = `
-      delete from "cartInventory"
-      where "cartId" = $1
-      returning *
-    `;
-    const params2 = [cart.cartId];
-    const result2 = await db.query(sql2, params2);
-    const deletedItems = result2.rows;
+    const deletedItems = result.rows;
 
     res.json(deletedItems);
   } catch (err) {
