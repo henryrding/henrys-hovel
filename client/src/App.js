3fb9537e7d43a4bf3c66f899b84e5e79f0e6508d
Henry Ding <henryrding@yahoo.com>
2023-05-05 00:01:03 +0000

    Updated App.js with add-inventory route and updated CardDetails path with type. #17

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-// import { useEffect, useState } from 'react';
 import {Route, Routes} from 'react-router-dom';
 import './App.css';
 import NavBar from './components/NavBar';
@@ -7,40 +6,25 @@ import Catalog from './pages/Catalog';
 import CardDetails from './pages/CardDetails';
 import Cart from './pages/Cart';
 import Auth from './pages/AuthPage';
-// import Inventory from './pages/Inventory';
+import AddInventory from './pages/AddInventory';
 import Orders from './pages/Orders';
 import OrderDetails from './pages/OrderDetails';
 import NotFound from './pages/NotFound';
 
 function App() {
-  // const [serverData, setServerData] = useState("");
-
-  // useEffect(() => {
-  //   async function getServerData() {
-  //     const resp = await fetch('/api/hello');
-  //     const data = await resp.json();
-
-  //     console.log('Data from server:', data);
-
-  //     setServerData(data.message);
-  //   }
-
-  //   getServerData();
-  // }, []);
-
 
   return(
     <ShopContextProvider>
       <Routes>
         <Route path="/" element={<NavBar />}>
           <Route index element={<Catalog />} />
-          <Route path="details/:cardId" element={<CardDetails />} />
+          <Route path="details/:cardId/:type" element={<CardDetails />} />
           <Route path="cart" element={<Cart />} />
           <Route path="sign-in" element={<Auth action="sign-in" />} />
           <Route path="sign-up" element={<Auth action="sign-up" />} />
           <Route path="orders" element={<Orders />} />
           <Route path="orderDetails/:orderNumber" element={<OrderDetails />} />
-          {/* <Route path="inventory" element={<Inventory />} /> */}
+          <Route path="add-inventory" element={<AddInventory />} />
           <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
