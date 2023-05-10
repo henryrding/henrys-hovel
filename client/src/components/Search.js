8d82bd8b7ac71171610cafb1dd456a0458ae9ecb
Henry Ding <henryrding@yahoo.com>
2023-05-10 06:25:14 +0000

    Updated Search useEffect to be more readable with new fetchApiResponse return. #18

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -17,10 +17,9 @@ export default function Search({ inventory, handleSearch }) {
           ) : (
             await fetchApiResponse(searchQuery)
           );
-          console.log(filteredResults?.data);
           setError();
-          setFilteredInventory(inventory ? filteredResults : filteredResults.data);
-          handleSearch(inventory ? filteredResults : filteredResults.data);
+          setFilteredInventory(filteredResults);
+          handleSearch(filteredResults);
         } catch (err) {
           setError(err);
         }
