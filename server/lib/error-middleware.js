6b01b35f361812483380b3391d7591cf2b32b6c6
Henry Ding <henryrding@yahoo.com>
2023-05-13 06:33:04 +0000

    Updated error-middleware to send error if card is already in inventory when trying to add to inventory. #18

--- server/lib/error-middleware.js
+++ server/lib/error-middleware.js
@@ -7,7 +7,11 @@ export default function errorMiddleware(err, req, res, next) {
   } else if (err instanceof jwt.JsonWebTokenError) {
     res.status(401).json({ error: 'invalid access token' });
   } else if (err.code === '23505') {
-    res.status(409).json({ error: 'username already exists' });
+    if (err.message.includes('inventory_card_finish_unique')) {
+      res.status(409).json({ error: 'this card is already in inventory' });
+    } else {
+      res.status(409).json({ error: 'username already exists' });
+    }
   } else {
     console.error(err);
     res.status(500).json({ error: 'an unexpected error occurred' });
