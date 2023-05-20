369c6e6e830f9a6e6c5f6e9d8eaa97b8dcde6e69
Henry Ding <henryrding@yahoo.com>
2023-05-20 06:09:52 +0000

    Updated index.js to export italicizeReminderText function. #19

--- client/src/lib/index.js
+++ client/src/lib/index.js
@@ -3,3 +3,4 @@ export { default as toDollars } from './to-dollars';
 export { default as createLineBreaks } from './create-line-breaks';
 export { default as handleToast } from './handle-toast' ;
 export { default as getTotalQuantity} from './get-total-quantity';
+export { default as italicizeReminderText} from './italicize-reminder-text';
